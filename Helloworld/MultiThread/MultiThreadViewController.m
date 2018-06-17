cb1b954aff7f982c48a8d7edcec8f05d846f6d63
yuzhiyunAtTencent <zhiyunyu@tencent.com>
2018-06-17 19:46:31 +0800

    GCD

--- Helloworld/MultiThread/MultiThreadViewController.m
+++ Helloworld/MultiThread/MultiThreadViewController.m
@@ -18,18 +18,30 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    [self executeNewThread];
+    
+
+    UIButton *btn1 = [[UIButton alloc] initWithFrame:CGRectMake(20, 80, [UIScreen mainScreen].bounds.size.width - 40, 40)];
+    [btn1 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [btn1 addTarget:self action:@selector(clickNSThread) forControlEvents:UIControlEventTouchUpInside];
+    [btn1 setTitle:@"NSThread" forState:UIControlStateNormal];
+    [self.view addSubview:btn1];
+    
+    UIButton *btn2 = [[UIButton alloc] initWithFrame:CGRectMake(20, 120, [UIScreen mainScreen].bounds.size.width - 40, 40)];
+    [btn2 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [btn2 addTarget:self action:@selector(clickGCD) forControlEvents:UIControlEventTouchUpInside];
+    [btn2 setTitle:@"GCD" forState:UIControlStateNormal];
+    [self.view addSubview:btn2];
 }
 
-- (void)executeNewThread {
+- (void)clickNSThread {
     // NSThread的3种使用方法
     
     // 1.通过alloc init的方式
-//    NSThread *thread = [[NSThread alloc] initWithTarget:self selector:@selector(run) object:nil];
-//    [thread start];
+    //    NSThread *thread = [[NSThread alloc] initWithTarget:self selector:@selector(run) object:nil];
+    //    [thread start];
     
     // 2.使用静态方法
-//    [NSThread detachNewThreadSelector:@selector(run) toTarget:self withObject:nil];
+    //    [NSThread detachNewThreadSelector:@selector(run) toTarget:self withObject:nil];
     
     // 3.使用NSObject(NSThreadPerformAdditions) 的分类创建
     [self performSelectorInBackground:@selector(run) withObject:nil];
@@ -54,7 +66,18 @@
 }
 
 - (void)runMainThread {
-    NSLog(@"回到主线程");
+    NSLog(@"我是主线程");
+}
+
+- (void)clickGCD {
+    [self runMainThread];
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        NSLog(@"执行子线程。。。");
+        [NSThread sleepForTimeInterval:2];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self runMainThread];
+        });
+    });
 }
 
 @end
