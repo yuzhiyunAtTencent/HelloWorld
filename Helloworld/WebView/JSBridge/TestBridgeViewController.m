707da99325ba73b4700ddf32b7e0bf70365de0eb
yuzhiyunAtTencent <zhiyunyu@tencent.com>
2018-08-30 19:53:14 +0800

    cr

--- Helloworld/WebView/JSBridge/TestBridgeViewController.m
+++ Helloworld/WebView/JSBridge/TestBridgeViewController.m
@@ -24,9 +24,9 @@
     // Do any additional setup after loading the view.
     
     [self.view addSubview:self.webView];
+    
     [self p_loadRequest];
     [self p_setupBridge];
-    
 }
 
 #pragma mark - WKNavigationDelegate
@@ -48,13 +48,12 @@
  */
 -(void)webView:(WKWebView *)webView runJavaScriptAlertPanelWithMessage:(NSString *)message initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(void))completionHandler {
     UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"提示" message:message?:@"" preferredStyle:UIAlertControllerStyleAlert];
+    
     [alertController addAction:([UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
         completionHandler();
-        NSLog(@"点击了取消按钮==%@",message);
     }])];
     [alertController addAction:([UIAlertAction actionWithTitle:@"确认" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
         completionHandler();
-        NSLog(@"点击了确定按钮==%@",message);
     }])];
     
     [self presentViewController:alertController animated:YES completion:nil];
@@ -82,7 +81,7 @@
                                                          ofType:@"html"];
     
     // 这是没用的 ,没有协议头 @"/Users/zhiyunyu/Library/Developer/CoreSimulator/Devices/EE71985E-5ACE-444A-A90F-FEDE64F242D3/data/Containers/Bundle/Application/011F773E-30CF-400B-8106-880E3F27407A/Helloworld.app/index.html"    0x000060400009c020
-    //    NSURL *urlWrong = [NSURL URLWithString:htmlPath];
+    // NSURL *urlWrong = [NSURL URLWithString:htmlPath];
     // 有协议头      @"file:///Users/zhiyunyu/Library/Developer/CoreSimulator/Devices/EE71985E-5ACE-444A-A90F-FEDE64F242D3/data/Containers/Bundle/Application/011F773E-30CF-400B-8106-880E3F27407A/Helloworld.app/index.html"    0x00006040000b8840
     NSURL *url = [NSURL fileURLWithPath:htmlPath];
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
