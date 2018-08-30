d093d07f739c0ac872b40e5ceba23faabb4f90ce
yuzhiyunAtTencent <zhiyunyu@tencent.com>
2018-08-30 16:55:57 +0800

    js调用native

--- Helloworld/WebView/JSBridge/TestBridgeViewController.m
+++ Helloworld/WebView/JSBridge/TestBridgeViewController.m
@@ -8,9 +8,13 @@
 
 #import "TestBridgeViewController.h"
 #import <WebKit/WebKit.h>
+#import "WebViewJavascriptBridge.h"
 
 @interface TestBridgeViewController () <WKUIDelegate, WKNavigationDelegate>
+
 @property(nonatomic, strong) WKWebView *webView;
+@property WebViewJavascriptBridge* bridge;
+
 @end
 
 @implementation TestBridgeViewController
@@ -19,25 +23,13 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    WKWebViewConfiguration *configuration =[[WKWebViewConfiguration alloc] init];
-    self.webView = [[WKWebView alloc] initWithFrame:CGRectMake(0, kQNNavigationBarHeight_DP + kQNSystemStatusBarHeight, SCREEN_WIDTH, SCREEN_HEIGHT) configuration:configuration];
-    self.webView.backgroundColor = [UIColor blueColor];
     [self.view addSubview:self.webView];
-    self.webView.UIDelegate = self;
-    self.webView.navigationDelegate = self;
+    [self p_loadRequest];
+    [self p_setupBridge];
     
-    NSString *htmlPath = [[NSBundle mainBundle] pathForResource:@"index"
-                                                          ofType:@"html"];
-    
-    // 这是没用的 ,没有协议头 @"/Users/zhiyunyu/Library/Developer/CoreSimulator/Devices/EE71985E-5ACE-444A-A90F-FEDE64F242D3/data/Containers/Bundle/Application/011F773E-30CF-400B-8106-880E3F27407A/Helloworld.app/index.html"    0x000060400009c020
-//    NSURL *urlWrong = [NSURL URLWithString:htmlPath];
-    // 有协议头      @"file:///Users/zhiyunyu/Library/Developer/CoreSimulator/Devices/EE71985E-5ACE-444A-A90F-FEDE64F242D3/data/Containers/Bundle/Application/011F773E-30CF-400B-8106-880E3F27407A/Helloworld.app/index.html"    0x00006040000b8840
-    NSURL *url = [NSURL fileURLWithPath:htmlPath];
-    NSURLRequest *request = [NSURLRequest requestWithURL:url];
-    [self.webView loadRequest:request];
 }
 
-#pragma mark-WKNavigationDelegate
+#pragma mark - WKNavigationDelegate
 
 - (void)webView:(WKWebView *)webView didFinishNavigation:(null_unspecified WKNavigation *)navigation {
     NSLog(@"load finish");
@@ -45,12 +37,11 @@
 
 
 - (void)webView:(WKWebView *)webView didFailNavigation:(null_unspecified WKNavigation *)navigation withError:(NSError *)error {
-    NSDictionary *dic = error.userInfo;
+//    NSDictionary *dic = error.userInfo;
     NSLog(@"error");
-    
 }
 
-#pragma mark-WKUIDelegate
+#pragma mark - WKUIDelegate
 
 /**
  *默认js不允许弹窗，所以需要自己提供原生窗口来支持弹窗
@@ -67,6 +58,44 @@
     }])];
     
     [self presentViewController:alertController animated:YES completion:nil];
+}
+
+#pragma mark - Accessors
+
+- (WKWebView *)webView {
+    if (!_webView) {
+        
+        WKWebViewConfiguration *configuration =[[WKWebViewConfiguration alloc] init];
+        _webView = [[WKWebView alloc] initWithFrame:CGRectMake(0, kQNNavigationBarHeight_DP + kQNSystemStatusBarHeight, SCREEN_WIDTH, SCREEN_HEIGHT) configuration:configuration];
+        _webView.backgroundColor = [UIColor blueColor];
+        _webView.UIDelegate = self;
+        _webView.navigationDelegate = self;
+    }
+    return _webView;
+}
+
+#pragma mark - Private
+
+- (void)p_loadRequest {
+    // 在这个网页中可以测试我们的Native接口
+    NSString *htmlPath = [[NSBundle mainBundle] pathForResource:@"index"
+                                                         ofType:@"html"];
+    
+    // 这是没用的 ,没有协议头 @"/Users/zhiyunyu/Library/Developer/CoreSimulator/Devices/EE71985E-5ACE-444A-A90F-FEDE64F242D3/data/Containers/Bundle/Application/011F773E-30CF-400B-8106-880E3F27407A/Helloworld.app/index.html"    0x000060400009c020
+    //    NSURL *urlWrong = [NSURL URLWithString:htmlPath];
+    // 有协议头      @"file:///Users/zhiyunyu/Library/Developer/CoreSimulator/Devices/EE71985E-5ACE-444A-A90F-FEDE64F242D3/data/Containers/Bundle/Application/011F773E-30CF-400B-8106-880E3F27407A/Helloworld.app/index.html"    0x00006040000b8840
+    NSURL *url = [NSURL fileURLWithPath:htmlPath];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [self.webView loadRequest:request];
+}
+
+- (void)p_setupBridge {
+    self.bridge = [WebViewJavascriptBridge bridgeForWebView:self.webView];
+
+    [self.bridge registerHandler:@"ObjC Echo" handler:^(id data, WVJBResponseCallback responseCallback) {
+        NSLog(@"ObjC Echo called with: %@", data);
+        responseCallback(@"I am native");
+    }];
 }
 
 @end
