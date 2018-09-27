0643aef01faad3878d0f6eea49570202ba2c393e
yuzhiyunAtTencent <zhiyunyu@tencent.com>
2018-09-27 16:18:47 +0800

    显示长图

---
+++ Helloworld/SDWebImage/LongImageView/YunLongImageView.m
@@ -0,0 +1,14 @@
+//
+//  YunLongImageView.m
+//  Helloworld
+//
+//  Created by zhiyunyu on 2018/9/27.
+//  Copyright © 2018 zhiyunyu. All rights reserved.
+//
+
+#import "YunLongImageView.h"
+
+@implementation YunLongImageView
+
+
+@end
--- Helloworld/SDWebImage/YunSDWebImageViewController.m
+++ Helloworld/SDWebImage/YunSDWebImageViewController.m
@@ -9,6 +9,8 @@
 #import "YunSDWebImageViewController.h"
 #import <SDWebImage/UIImageView+WebCache.h>
 
+#define IMAGE_URL @"http://puep.qpic.cn/coral/Q3auHgzwzM4fgQ41VTF2rJVmkJxEfgQqn3tmZja9RibGElm27800WvQ/640"
+
 @interface YunSDWebImageViewController ()
 
 @end
@@ -19,12 +21,36 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 100, SCREEN_WIDTH, 400)];
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 100, SCREEN_WIDTH, 500)];
+    imageView.contentMode = UIViewContentModeScaleAspectFill;
+    imageView.clipsToBounds = YES;
     [self.view addSubview:imageView];
+    [imageView sd_setImageWithURL:[NSURL URLWithString:IMAGE_URL]];
     
-    [imageView sd_setImageWithURL:[NSURL URLWithString:@"https://raw.githubusercontent.com/rs/SDWebImage/master/Docs/SDWebImageClassDiagram.png"]];
-    
+    [imageView setUserInteractionEnabled:YES];
+    UITapGestureRecognizer *gesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(showCompleteImageView)];
+    [imageView addGestureRecognizer:gesture];
+
 }
+
+- (void)showCompleteImageView {
+    [self addImageOnViewWithImage];
+}
+
+-(void)addImageOnViewWithImage{
+    UIScrollView *scroll = [[UIScrollView alloc]initWithFrame:self.view.frame];
+    scroll.showsVerticalScrollIndicator = YES;
+    [self.view addSubview:scroll];
+
+    // 这个长图的高度一般来说是服务器下发的
+    scroll.contentSize = CGSizeMake(SCREEN_WIDTH, 1400);
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0,0,SCREEN_WIDTH, 1400)];
+    imageView.contentMode = UIViewContentModeScaleAspectFill;
+    [imageView sd_setImageWithURL:[NSURL URLWithString:IMAGE_URL]];
+    
+    [scroll addSubview:imageView];
+}
+    
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
