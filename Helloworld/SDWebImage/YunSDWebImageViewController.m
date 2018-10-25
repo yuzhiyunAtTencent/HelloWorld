ed582d8abc8f4f00c738d72910cfd7966999dedf
yuzhiyunAtTencent <zhiyunyu@tencent.com>
2018-10-25 20:29:43 +0800

    在test分支第二次追加代码

--- Helloworld/SDWebImage/YunSDWebImageViewController.m
+++ Helloworld/SDWebImage/YunSDWebImageViewController.m
@@ -21,6 +21,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     // 在test分支追加代码
+    // 在test分支第二次追加代码
     
     UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 100, SCREEN_WIDTH, 500)];
     imageView.contentMode = UIViewContentModeScaleAspectFill;
